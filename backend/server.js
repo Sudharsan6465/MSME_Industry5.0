7475476d8e20730c2522ead2cd94b131aab69a86
sriramsivacoder <your-email@example.com>
2026-07-22 19:06:02 +0530

    Added first api

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-Hello
+const express=require('express')
+
+
+const app=express()
+
+const PORT=3000
+
+app.get('/api',(req,res)=>{
+    res.json({message:"This text from the Node Server.... "})
+})
+
+app.listen(PORT,()=>{console.log("The server is now running...")})
